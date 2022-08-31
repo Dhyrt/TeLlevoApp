fe5c5a33410b9253699cf15a0b1ee3d6228557a7
Dhyrt <caro.ahumada@duocuc.cl>
2022-08-31 17:11:30 -0400

    commit

--- src/app/services/usuarios.service.ts
+++ src/app/services/usuarios.service.ts
@@ -5,5 +5,62 @@ import { Injectable } from '@angular/core';
 })
 export class UsuariosService {
 
+  usuarios: any[] = []
+  vehiculos: any[] = []
+
   constructor() { }
+
+  //Metodos usuario
+  agregarUsuario(usuario): boolean{
+    if ( this.obtenerUsuario(usuario.rut) == undefined ) {
+      this.usuarios.push(usuario);
+      return true;
+    }
+    return false;
+  }
+  eliminarUsuario(rut: string){
+    this.usuarios.forEach((usu, index) => {
+      if (usu.rut == rut) {
+        this.usuarios.splice(index, 1);
+      }
+    });
+  }
+  actualizarUsuario(usuario){
+    var index = this.usuarios.findIndex(usu => usu.rut == usuario.rut);
+    this.usuarios[index] = usuario;
+  }
+  obtenerUsuario(rut: string){
+    return this.usuarios.find(usu => usu.rut == rut);
+  }
+  obtenerUsuarios(){
+    return this.usuarios;
+  }
+
+  //Metodos Vehiculo
+  agregarVehiculo(vehiculo): boolean{
+    if ( this.obtenerVehiculo(vehiculo.patente) == undefined ) {
+      this.vehiculos.push(vehiculo);
+      return true;
+    }
+    return false;
+  }
+  eliminarVehiculo(patente: string){
+    this.vehiculos.forEach((auto, index) => {
+      if (auto.patente == patente) {
+        this.vehiculos.splice(index, 1);
+      }
+    });
+  }
+  actualizarVehiculo(vehiculo){
+    var index = this.vehiculos.findIndex(auto => auto.rut == vehiculo.patente);
+    this.vehiculos[index] = vehiculo;
+  }
+  obtenerVehiculo(patente: string){
+    return this.vehiculos.find(auto => auto.patente == patente);
+  }
+
+  //Login
+  validarLogin(nombre, password){
+    return this.usuarios.find(usu => usu.nombre == nombre && usu.password == password);
+  }
 }
