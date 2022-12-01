e59e9827ed6c9e8218baa6e2f233af46c141068a
donjhonatan <jh.quiroga@duocuc.cl>
2022-11-30 21:10:46 -0300

    123

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyB-0Z9WBRY_ZZ7gWWPNTsC9txWU7UsWsZY",
+    authDomain: "tellevoapp-4d3fa.firebaseapp.com",
+    projectId: "tellevoapp-4d3fa",
+    storageBucket: "tellevoapp-4d3fa.appspot.com",
+    messagingSenderId: "375378574295",
+    appId: "1:375378574295:web:961e66406dede3e5f7a992"
+  }
 };
